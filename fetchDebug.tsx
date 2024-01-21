6508e8788a10cabd8397d41d6359d0a9c3fb42d0
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-22 01:55:42 +0800

    Update fetchDebug.tsx

--- fetchDebug.tsx
+++ fetchDebug.tsx
@@ -1,21 +1,25 @@
+// 异步函数，发送一个用于调试的请求
 export async function fetchDebug(
-    input: RequestInfo | URL,
-    init?: RequestInit | undefined,
+    input: RequestInfo | URL, // 请求的URL或URL对象
+    init?: RequestInit | undefined // 请求的初始化配置，可选参数
 ): Promise<Response> {
-    const request = new Request(input, init);
-    const { url, method, headers } = request;
+    // 返回一个Promise<Response>
+    const request = new Request(input, init); // 创建一个Request对象
+    const { url, method, headers } = request; // 获取URL、请求方法和请求头信息
     console.log("request", {
+        // 打印请求信息
         url,
         method,
         headers: Object.fromEntries(headers),
     });
-    const response = await fetch(request);
+    const response = await fetch(request); // 发送请求并获取响应
 
     console.log("response", {
-        url: request.url,
-        status: response.status,
-        method,
-        headers: Object.fromEntries(response.headers),
+        // 打印响应信息
+        url: request.url, // 请求的URL
+        status: response.status, // 响应状态码
+        method, // 请求方法
+        headers: Object.fromEntries(response.headers), // 响应头信息
     });
-    return response;
+    return response; // 返回响应对象
 }
