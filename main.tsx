9dfe827a31b475b30d808ab22b87952631d64971
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-22 02:00:45 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,15 +1,27 @@
+// 导入所需模块
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
 import { parse } from "https://deno.land/std@0.182.0/flags/mod.ts";
 import { handler } from "./handler.tsx";
+
+// 如果当前脚本是作为主脚本运行
 if (import.meta.main) {
+    // 解析命令行参数，并获取端口号和主机名
     let { port, hostname } = parse(Deno.args);
+
+    // 如果端口号或主机名存在
     if (port || hostname) {
+        // 如果端口号不存在，则设置默认端口号为8000
         port ??= 8000;
+        // 如果主机名不存在，则设置默认主机名为0.0.0.0
         hostname ??= "0.0.0.0";
+
+        // 启动服务器，监听指定的端口号和主机名
         await serve(handler, { port, hostname });
     } else {
-        //         console.log("Listening on http://localhost:8000");
-
-        await serve(handler, { hostname: "0.0.0.0", port: 8000 });
+        // 否则，使用默认的端口号8000和主机名0.0.0.0监听
+        await Promise.all([
+            serve(handler, { hostname: "::", port: 8000 }),
+            serve(handler, { hostname: "0.0.0.0", port: 8000 }),
+        ]);
     }
 }
