3d809c3aebba502341023e28f0448917b84c8d3a
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-14 10:45:38 +0800

    Update get_doh_url.tsx

--- get_doh_url.tsx
+++ get_doh_url.tsx
@@ -6,7 +6,7 @@ export function get_doh_url(): string {
     if(Deno.env.get("doh").startsWith("[")&&Deno.env.get("doh").endsWith("]")){
 const dohs=JSON.parse(Deno.env.get("doh"))
 
-if(dohs.length)        return dohs[Math.floor(dohs.length*Math.random())]
+if(dohs.length)        return new URL(dohs[Math.floor(dohs.length*Math.random())]).href
     }
     return new URL(Deno.env.get("doh") ?? "https://dns.alidns.com/dns-query")
         .href;
