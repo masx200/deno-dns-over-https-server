329a59318cc2ac051f6d81c9717b2e030d657570
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-14 10:44:36 +0800

    Update get_doh_url.tsx

--- get_doh_url.tsx
+++ get_doh_url.tsx
@@ -3,6 +3,11 @@
  * @returns {string} DoH的URL地址
  */
 export function get_doh_url(): string {
+    if(Deno.env.get("doh").startsWith("[")&&Deno.env.get("doh").endsWith("]")){
+const dohs=JSON.parse(Deno.env.get("doh"))
+
+if(dohs.length)        return dohs[Math.floor(dohs.length*Math.random())]
+    }
     return new URL(Deno.env.get("doh") ?? "https://dns.alidns.com/dns-query")
         .href;
 }
