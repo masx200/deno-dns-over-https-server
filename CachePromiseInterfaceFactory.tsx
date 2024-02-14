50e82f1d5d9edb1884e88eefba55eeae1f7d8671
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-14 11:06:58 +0800

    Update CachePromiseInterfaceFactory.tsx

--- CachePromiseInterfaceFactory.tsx
+++ CachePromiseInterfaceFactory.tsx
@@ -26,7 +26,7 @@ export async function CachePromiseInterfaceFactory(): Promise<
                 result.value.ttl
             ) {
                 console.log(
-                    JSON.stringify({ key, value: result.value }, null, 4),
+                    JSON.stringify({ function:"get",key, value: result.value }, null, 4),
                 );
                 return result.value;
             } else {
@@ -43,7 +43,7 @@ export async function CachePromiseInterfaceFactory(): Promise<
                     "Failed to set " + key + " to " + JSON.stringify(value),
                 );
             }
-            console.log(JSON.stringify({ key, value }, null, 4));
+            console.log(JSON.stringify({function:"set", key, value }, null, 4));
         },
     } satisfies CachePromiseInterface;
 }
