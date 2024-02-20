70d0e87261d6fe0be9d9534a951f1bd010a221c7
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-20 22:57:11 +0800

    Update parse_dns_message.tsx

--- parse_dns_message.tsx
+++ parse_dns_message.tsx
@@ -2,11 +2,27 @@ import {
     Context,
     NextFunction,
     RetHandler,
+    bodyToBuffer,
 } from "https://cdn.jsdelivr.net/gh/masx200/deno-http-middleware@3.3.0/mod.ts";
+import { dns_query_path_name } from "./dns_query_path_name.tsx";
+import { get_path_name } from "./get_path_name.tsx";
 
 export async function parse_dns_message(
-    context: Context,
-    next: NextFunction,
+    ctx: Context,
+    next: NextFunction
 ): Promise<RetHandler> {
-    return await next();
+    const req = ctx.request;
+    const { url } = req;
+    const pathname = get_path_name(url);
+    if (
+        pathname === dns_query_path_name() &&
+        (ctx.request.method === "POST" || ctx.request.method === "GET")
+    ) {
+        const body = req.body && (await bodyToBuffer(req.body));
+        req.body = body;
+
+        console.log({ body });
+    } else {
+        return await next();
+    }
 }
