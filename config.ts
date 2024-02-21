5a35e8670c0a4e51b6b709bd9fd38819367666d0
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-22 00:22:55 +0800

    getAllTailscaleNetworkIPsAndSelfPublicIPs

--- config.ts
+++ config.ts
@@ -2,23 +2,16 @@ const config: Record<string, string[]> = {
     "desktop-5rh4po7.manx-sun.ts.net": [
         "100.127.196.48",
         "fd7a:115c:a1e0:ab12:4843:cd96:627f:c430",
+        "114.84.158.141",
+        "240e:38b:892f:e800:1c88:6f7:d715:c199",
     ],
-    "openwrt.manx-sun.ts.net": [
-        "100.112.199.76",
-        "fd7a:115c:a1e0::6530:c74c",
-    ],
-    "openwrt-2.manx-sun.ts.net": [
-        "100.81.33.34",
-        "fd7a:115c:a1e0::4a91:2122",
-    ],
+    "openwrt.manx-sun.ts.net": ["100.112.199.76", "fd7a:115c:a1e0::6530:c74c"],
+    "openwrt-2.manx-sun.ts.net": ["100.81.33.34", "fd7a:115c:a1e0::4a91:2122"],
     "xiaomi-23013rk75c.manx-sun.ts.net": [
         "100.79.244.87",
         "fd7a:115c:a1e0::f88f:f457",
     ],
-    "openwrt-1.manx-sun.ts.net": [
-        "100.86.137.31",
-        "fd7a:115c:a1e0::9b56:891f",
-    ],
+    "openwrt-1.manx-sun.ts.net": ["100.86.137.31", "fd7a:115c:a1e0::9b56:891f"],
     "xiaomi-m2007j3sc.manx-sun.ts.net": [
         "100.92.232.23",
         "fd7a:115c:a1e0::df5c:e817",
--- get_all_tailscale_ips.ts
+++ get_all_tailscale_ips.ts
@@ -1,6 +1,9 @@
 import { runCommand } from "./runCommand.ts";
-
-export async function getAllTailscaleNetworkIPs(): Promise<
+import {
+    getPublicIpv4,
+    getPublicIpv6,
+} from "https://deno.land/x/masx200_get_public_ip_address@1.0.4/mod.ts";
+export async function getAllTailscaleNetworkIPsAndSelfPublicIPs(): Promise<
     { [x: string]: string[] }
 > {
     const text = await runCommand("tailscale", ["status", "--json"]);
@@ -9,8 +12,9 @@ export async function getAllTailscaleNetworkIPs(): Promise<
         Peer: Record<string, { DNSName: string; TailscaleIPs: string[] }>;
     } = JSON.parse(text);
 
+    const selfIPs = Array.from(data.Self.TailscaleIPs);
     const config = {
-        [data.Self.DNSName.slice(0, -1)]: data.Self.TailscaleIPs,
+        [data.Self.DNSName.slice(0, -1)]: selfIPs,
     };
 
     for (const v of Object.values(data.Peer)) {
@@ -18,7 +22,21 @@ export async function getAllTailscaleNetworkIPs(): Promise<
             [v.DNSName.slice(0, -1)]: v.TailscaleIPs,
         });
     }
+    try {
+        selfIPs.push(await getPublicIpv4());
+    } catch (error) {
+        console.error(error);
+    }
+    try {
+        selfIPs.push(await getPublicIpv6());
+    } catch (error) {
+        console.error(error);
+    }
     // console.log(JSONSTRINGIFYNULL4(config, null, 4))
     return config;
 }
-// console.log(await getAllTailscaleNetworkIPs());
+
+if (import.meta.main) {
+    console.log(await getAllTailscaleNetworkIPsAndSelfPublicIPs());
+}
+// console.log(await getAllTailscaleNetworkIPsAndSelfPublicIPs());
