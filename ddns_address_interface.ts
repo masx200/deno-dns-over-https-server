bcffd9c8524bd323dbfbfe3c68eb208b596e91d8
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-22 12:04:18 +0800

    Create ddns_address_interface.ts

---
+++ ddns_address_interface.ts
@@ -0,0 +1,51 @@
+/**
+ * 响应缓存类型接口
+ */
+export type DDNScontentType =
+    & DDNScontentID
+    & DDNScontentContent;
+export type DDNScontentContent = {
+    /**DNS record name (or @ for the zone apex) in Punycode. */
+    name: string;
+    /**A valid IPv4 address. */
+    content: string;
+    /**Record type. */
+    type: string;
+};
+export interface DDNScontentID {
+    /** Identifier */
+    id: string;
+}
+// 缓存Promise接口
+export type DDNScontentInterface = {
+    /**List, search, sort, and filter a zones' DNS records. */
+    ListDNSRecords(
+        options: { name?: string; content?: string },
+    ): Promise<DDNScontentType[]>;
+    /**Create a new DNS record for a zone.
+
+Notes:
+
+A/AAAA records cannot exist on the same name as CNAME records.
+NS records cannot exist on the same name as any other record type.
+Domain names are always represented in Punycode, even if Unicode characters were used when creating the record. */
+    CreateDNSRecord(
+        record: DDNScontentContent[],
+    ): Promise<DDNScontentType[]>;
+    /**Overwrite an existing DNS record. Notes:
+
+A/AAAA records cannot exist on the same name as CNAME records.
+NS records cannot exist on the same name as any other record type.
+Domain names are always represented in Punycode, even if Unicode characters were used when creating the record. */
+    OverwriteDNSRecord(array: DDNScontentType[]): Promise<DDNScontentType[]>;
+    /** Update an existing DNS record. Notes:
+
+A/AAAA records cannot exist on the same name as CNAME records.
+NS records cannot exist on the same name as any other record type.
+Domain names are always represented in Punycode, even if Unicode characters were used when creating the record. */
+    UpdateDNSRecord(array: DDNScontentType[]): Promise<DDNScontentType[]>;
+    /**Delete DNS Record  */
+    DeleteDNSRecord(array: DDNScontentID[]): Promise<DDNScontentID[]>;
+    /**DNS Record Details */
+    DNSRecordDetails(array: DDNScontentID[]): Promise<DDNScontentType[]>;
+};
