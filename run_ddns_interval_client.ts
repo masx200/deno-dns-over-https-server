a1e44d76fa8616e9fedd1b5844bd07f5aee397d0
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-23 16:00:11 +0800

    Update run_ddns_interval_client.ts

--- run_ddns_interval_client.ts
+++ run_ddns_interval_client.ts
@@ -7,7 +7,7 @@ import parse from "npm:@masx200/mini-cli-args-parser@1.1.0";
 import { DDNScontentContent } from "./DDNScontentContent.ts";
 import { getAllTailscaleNetworkIPsAndSelfPublicIPs } from "./get_all_tailscale_ips.ts";
 
-export function run_ddns_interval_client(
+export async function run_ddns_interval_client(
     opts: {
         interval: number;
         ipv4: boolean;
@@ -18,24 +18,18 @@ export function run_ddns_interval_client(
         name: string;
         service_url: string;
     },
-): () => void {
+): Promise<() => void> {
+    await run_ddns_update_once(opts);
     const timer = setInterval(async () => {
-        const dnscontents: DDNScontentContent[] =
-            await getAllTailscaleNetworkIPsAndSelfPublicIPs({
-                name: opts.name,
-                public: opts.public,
-                tailscale: opts.tailscale,
-                ipv4: opts.ipv4,
-                ipv6: opts.ipv6,
-            });
-        console.log(dnscontents);
+        await run_ddns_update_once(opts);
     }, opts.interval);
     return () => clearInterval(timer);
 }
 
 if (import.meta.main) {
     const opts = parse(Deno.args);
-    const stop = run_ddns_interval_client({
+    console.log(opts);
+    const stop = await run_ddns_interval_client({
         interval: Number(opts.interval || 30 * 1000),
         ipv4: Boolean(opts.ipv4 ?? true),
         ipv6: Boolean(opts.ipv6 ?? true),
@@ -46,4 +40,27 @@ if (import.meta.main) {
         service_url: opts.service_url ?? "XXXXXXXXXXXXXXXXXXXXX",
     });
     setTimeout(() => stop(), 10000);
+}
+
+async function run_ddns_update_once(
+    opts: {
+        interval: number;
+        ipv4: boolean;
+        ipv6: boolean;
+        tailscale: boolean;
+        public: boolean;
+        token: string;
+        name: string;
+        service_url: string;
+    },
+) {
+    const dnscontents: DDNScontentContent[] =
+        await getAllTailscaleNetworkIPsAndSelfPublicIPs({
+            name: opts.name,
+            public: opts.public,
+            tailscale: opts.tailscale,
+            ipv4: opts.ipv4,
+            ipv6: opts.ipv6,
+        });
+    console.log(dnscontents);
 }
