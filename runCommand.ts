5cd291df237ddf920747096fc40e37f61232d177
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-23 16:19:02 +0800

    Update runCommand.ts

--- runCommand.ts
+++ runCommand.ts
@@ -1,17 +1,23 @@
+/**
+ * 运行命令并返回结果
+ * @param cmd 命令字符串
+ * @param args 命令参数数组
+ * @returns 返回命令执行结果
+ */
 export async function runCommand(cmd: string, args: string[]): Promise<string> {
+    // 创建命令对象
     const command = new Deno.Command(cmd, {
         args: args,
     });
+    // 执行命令并获取输出结果
     const { code, stdout, stderr } = await command.output();
-    // console.assert(code === 0);
-    // console.assert("hello\n" === new TextDecoder().decode(stdout));
-    // console.assert("world\n" === new TextDecoder().decode(stderr));
+    // 如果命令执行失败，则抛出异常
     if (code !== 0) {
-        // console.log("Error: " + new TextDecoder().decode(stderr));
         throw new Error(
             `Command failed with exit code ${code} . Error:` +
                 new TextDecoder().decode(stderr),
         );
     }
+    // 返回命令执行结果
     return new TextDecoder().decode(stdout);
 }
