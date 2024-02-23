fade6e498ee7c91b3a1737fb5b5cd9f06e31dd8b
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-23 15:06:14 +0800

    Update get_all_tailscale_ips.ts

--- get_all_tailscale_ips.ts
+++ get_all_tailscale_ips.ts
@@ -5,34 +5,46 @@ import {
     getPublicIpv4,
     getPublicIpv6,
 } from "https://deno.land/x/masx200_get_public_ip_address@1.0.4/mod.ts";
-export async function getAllTailscaleNetworkIPsAndSelfPublicIPs(): Promise<
+export async function getAllTailscaleNetworkIPsAndSelfPublicIPs(
+    opts: { name: string; public: boolean; tailscale: boolean },
+): Promise<
     DDNScontentContent[]
 > {
-    const text = await runCommand("tailscale", ["status", "--json"]);
-    const data: {
-        Self: { DNSName: string; TailscaleIPs: string[] };
-        Peer: Record<string, { DNSName: string; TailscaleIPs: string[] }>;
-    } = JSON.parse(text);
-
-    const selfIPs = Array.from(data.Self.TailscaleIPs);
+    const { name, tailscale } = opts;
+    const selfIPs: string[] = [];
     const config = {
-        [data.Self.DNSName.slice(0, -1)]: selfIPs,
+        [name]: selfIPs,
     };
 
-    for (const v of Object.values(data.Peer)) {
-        Object.assign(config, {
-            [v.DNSName.slice(0, -1)]: v.TailscaleIPs,
-        });
+    if (tailscale) {
+        const text = await runCommand("tailscale", ["status", "--json"]);
+        const data: {
+            Self: { DNSName: string; TailscaleIPs: string[] };
+            // Peer: Record<string, { DNSName: string; TailscaleIPs: string[] }>;
+        } = JSON.parse(text);
+        selfIPs.push(...Array.from(data.Self.TailscaleIPs));
+        const tailscale_name = tailscale
+            ? data.Self.DNSName.slice(0, -1)
+            : name;
+        config[tailscale_name] = selfIPs;
     }
-    try {
-        selfIPs.push(await getPublicIpv4());
-    } catch (error) {
-        console.error(error);
-    }
-    try {
-        selfIPs.push(await getPublicIpv6());
-    } catch (error) {
-        console.error(error);
+
+    // for (const v of Object.values(data.Peer)) {
+    //     Object.assign(config, {
+    //         [v.DNSName.slice(0, -1)]: v.TailscaleIPs,
+    //     });
+    // }
+    if (opts.public) {
+        try {
+            selfIPs.push(await getPublicIpv4());
+        } catch (error) {
+            console.error(error);
+        }
+        try {
+            selfIPs.push(await getPublicIpv6());
+        } catch (error) {
+            console.error(error);
+        }
     }
     // console.log(JSONSTRINGIFYNULL4(config, null, 4))
     // return config;
