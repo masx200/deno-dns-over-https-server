1712337270b1f760a054ce2ba36810940d8e925f
masx200 <34191203+masx200@users.noreply.github.com>
2024-03-19 22:36:53 +0800

    Update Strict_Transport_Security.tsx

--- Strict_Transport_Security.tsx
+++ Strict_Transport_Security.tsx
@@ -19,7 +19,7 @@ export async function Strict_Transport_Security(
     await next();
     const headers = new Headers(ctx.response.headers);
 
-    headers.set("Strict-Transport-Security", "max-age=31536000");
+    headers.append("Strict-Transport-Security", "max-age=31536000");
     // console.log(ctx.response.body);
     // 必须把响应的主体转换为Uint8Array才行
     const body = ctx.response.body && (await bodyToBuffer(ctx.response.body));
